acb69923cab0495fd91273dbdb2391ec46a6ee30
Justin <>
2022-05-06 13:37:23 +0200

    Finished youtube tutorial Bedimcode

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,6 +81,42 @@ window.addEventListener("scroll", scrollUp);
 
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light"
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun"
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
+    themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](iconTheme)
+}
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem("selected-theme", getCurrentTheme())
+    localStorage.setItem("selected-icon", getCurrentIcon())
+})
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__title, .popular__container, .subscribe__container, .footer__container')
+sr.reveal('.home__description, .footer__info', {delay: 500})
+sr.reveal('.home__search', {delay: 600})
+sr.reveal('.home__value', {delay: 700})
+sr.reveal('.home__images', {delay: 800, origin: 'bottom'})
+sr.reveal('.logos__img', {interval: 100})
+sr.reveal('.values__images, .contact__content', {origin: 'left'})
+sr.reveal('.value__content, .contact__images', {origin: 'right'})
